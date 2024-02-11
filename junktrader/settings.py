33fb523a5580af26d663b923f627df3206a0390c
Hayespace <ben.hayes@hotmail.com>
2024-02-11 13:54:29 +0000

    add home templates

--- junktrader/settings.py
+++ junktrader/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -44,7 +45,10 @@ ROOT_URLCONF = 'junktrader.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
