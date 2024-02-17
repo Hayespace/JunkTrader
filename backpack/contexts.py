d440fdf092de8f3072cf8b125e70de816284da67
Hayespace <ben.hayes@hotmail.com>
2024-02-17 13:01:30 +0000

    updated context prcessor

--- backpack/contexts.py
+++ backpack/contexts.py
@@ -1,7 +1,21 @@
+from django.shortcuts import get_object_or_404
+from collectables.models import Collectable
+
 def backpack_contents(request):
     backpack_items = []
     total = 0
     collectable_count = 0
+    backpack = request.session.get('backpack', {})
+
+    for item_id, quantity in backpack.items():
+        collectable = get_object_or_404(Collectable, pk=item_id)
+        total += quantity * collectable.price
+        collectable_count += quantity
+        backpack_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'collectable': collectable,
+        })
     
     grand_total = total  # Assuming there's no additional delivery cost
 
