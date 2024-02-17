700334168710e8c9c3d0d6dd5cc8ee0302ebfd9a
Hayespace <ben.hayes@hotmail.com>
2024-02-17 11:32:07 +0000

    define backpack

--- backpack/views.py
+++ backpack/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def open_backpack(request):
+    """ A view that renders the backpack contents page """
+
     return render(request, 'backpack/backpack.html')
+
+def add_to_backpack(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    # Get backpack from session or initialize it as an empty dictionary
+    backpack = request.session.get('backpack', {})
+
+    if item_id in backpack:
+        backpack[item_id] += quantity
+    else:
+        backpack[item_id] = quantity
+
+    request.session['backpack'] = backpack
+    print(request.session['backpack'])
+    return redirect(redirect_url)
+
+
