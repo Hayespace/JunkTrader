771c59871e632ef292fd81536e0f9c8c289b03ad
Hayespace <ben.hayes@hotmail.com>
2024-02-17 11:43:33 +0000

    added backpack template

--- backpack/urls.py
+++ backpack/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.open_backpack, name='open_backpack'),
-    path('add/<item_id>', views.add_to_backpack, name='add_to_backpack'),
+    path('', views.open_backpack, name='open_backpack')
     
 ]
--- backpack/views.py
+++ backpack/views.py
@@ -3,23 +3,4 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def open_backpack(request):
-    
-
     return render(request, 'backpack/backpack.html')
-
-def add_to_backpack(request, item_id):
-    
-
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    backpack = request.session.get('backpack', {})
-
-    if item_id in list(backpack.keys()):
-        backpack[item_id] += quantity
-    else:
-        backpack[item_id] = quantity
-
-    request.session['backpack'] = backpack
-    print(request.session['backpack'])
-    return redirect(redirect_url)
-    
