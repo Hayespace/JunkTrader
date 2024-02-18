2c53805222c5c3e509908a1a361a176fca02e880
Hayespace <ben.hayes@hotmail.com>
2024-02-18 20:19:01 +0000

    add glb viewer

--- collectables/admin.py
+++ collectables/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from .models import Collectable
 
-# Register your models here.
-
 class CollectableAdmin(admin.ModelAdmin):
     list_display = (
         'id',
         'name',
         'price',
         'image',
+        'glb_file',
     )
 
     ordering = ('id',)
--- collectables/models.py
+++ collectables/models.py
@@ -1,13 +1,12 @@
-
 from django.db import models
 
 class Collectable(models.Model):
-    
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    glb_file = models.FileField(upload_to='glb_files/', null=True, blank=True)
 
     def __str__(self):
         return self.name
