fcf6aa8b220d3ff68184c0b3655ef72db12d53ba
Hayespace <ben.hayes@hotmail.com>
2024-03-03 01:41:26 +0000

    adjust upgrades page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('start_game/', views.index, name='start_game'),
+    path('reset-session/', views.reset_session, name='reset_session'),
    
 ]
