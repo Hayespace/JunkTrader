d7c7ec227f44d99b67ba2d7ad95c779e1f295b3a
Hayespace <ben.hayes@hotmail.com>
2024-04-01 11:39:00 +0000

    remove invalid column from models

--- profiles/models.py
+++ profiles/models.py
@@ -18,10 +18,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-    else:
-        # Existing users: just save the profile
-        if hasattr(instance, 'userprofile'):
-            instance.userprofile.save()
-        else:
-            # If UserProfile doesn't exist, create it
-            UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
