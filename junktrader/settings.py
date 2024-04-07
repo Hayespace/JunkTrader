89d923112af112c14c408f649f1c434cb109eeab
Hayespace <ben.hayes@hotmail.com>
2024-04-07 12:32:44 +0000

    set debug to environ

--- junktrader/settings.py
+++ junktrader/settings.py
@@ -7,11 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-
-
-#DEBUG = 'DEVELOPMENT' in os.environ
-
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'junktrader.herokuapp.com',
